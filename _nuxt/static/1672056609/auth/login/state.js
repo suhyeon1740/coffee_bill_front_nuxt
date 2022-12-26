b20d8ddde535259c521e7927f1a59fc57cf025d5
suhyeon1740 <suhyeon1740@users.noreply.github.com>
2022-12-26 12:10:10 +0000

    deploy: f1da4c1525ccb208b7fc94f6fdaa5b6ce22f3a65

---
+++ _nuxt/static/1672056609/auth/login/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcoffee_bill_front_nuxt\u002F_nuxt\u002Fstatic\u002F1672056609",layout:"default",error:a,serverRendered:true,routePath:"\u002Fauth\u002Flogin",config:{_app:{basePath:"\u002Fcoffee_bill_front_nuxt\u002F",assetsPath:"\u002Fcoffee_bill_front_nuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
